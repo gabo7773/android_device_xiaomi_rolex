830825682cf5f0ea3bea02b48081c1a7de383452
Bowgo Tsai <bowgotsai@google.com>
2019-04-08 18:28:48 +0800

    rolex: Adding GSI keys
    Change-Id: I112ece91566e6cea066cd637bfed5cae6761ae66

--- lineage_rolex.mk
+++ lineage_rolex.mk
@@ -23,6 +23,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Installs gsi keys into ramdisk, to boot a GSI with verified  boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Inherit from rolex device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
